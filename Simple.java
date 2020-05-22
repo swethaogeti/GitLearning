ed72e9b64a84ccba498527da0600ca1cdfe2773a
swethaogeti <65771591+swethaogeti@users.noreply.github.com>
2020-05-22 23:00:59 +0530

    thrid commit

---
+++ Simple.java
@@ -0,0 +1,9 @@
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello");
+System.out.println("s");
+System.out.println("learning git");
+Sysout.out.println("made changes in dev");
+System.out.println("again made changes in dev");  
+    }  
+}  
